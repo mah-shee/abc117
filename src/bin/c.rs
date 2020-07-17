947d374218a2940b6e290ed97d942afe56819190
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-17 23:33:23 +0900

    Initial commit

---
+++ src/bin/c.rs
@@ -0,0 +1,11 @@
+#[allow(unused_imports)]
+use proconio::marker::{Bytes, Chars, Usize1};
+use proconio::{fastout, input};
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        x: [usize; m]
+    }
+}
